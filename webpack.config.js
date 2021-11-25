9375b182f4d6767dad1c0fc83a7dfa017a7d33f1
Zmiter <ozmiter@gmail.com>
2021-11-25 16:29:10 +0300

    refactor: add webpack.config.js and prod/dev functional

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = [{
+  mode: 'development',
+  name: 'dev',
+  target: 'node',
+  entry: {
+    server: './src/simple-crud-api.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+},
+{
+  mode: 'production',
+  name: 'prod',
+  target: 'node',
+  entry: {
+    bundle: './src/simple-crud-api.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}];
