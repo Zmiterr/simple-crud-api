d4c6e79ff21077f452f66aa86e835c84cedbc9ab
Zmiter <ozmiter@gmail.com>
2021-11-28 22:23:53 +0300

    feat: make empty in memory base

--- src/persons.js
+++ src/persons.js
@@ -1,10 +1,10 @@
 const persons = [
-  {
-    id: '61eb8e5e-b734-4296-98f4-40aec1e6606c',
-    name: 'Ivan',
-    age: 18,
-    hobbies: ['guitar', 'codding'],
-  },
+  // {
+  //   id: '61eb8e5e-b734-4296-98f4-40aec1e6606c',
+  //   name: 'Ivan',
+  //   age: 18,
+  //   hobbies: ['guitar', 'codding'],
+  // },
 ];
 
 module.exports = persons;
